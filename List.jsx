a88d0bb05deb4dd7b149d132b761b946d27e757b
Saif <kmohdsaif40@gmail.com>
2021-07-26 11:08:44 +0530

    adding files

---
+++ List.jsx
@@ -0,0 +1,12 @@
+function List(props)
+{
+        return(
+            <ul>
+                {
+                    props.tasks.map((el, index)=>{
+                        return <ListItem removeTask={props.removeTaskHandler} key={index} taskVal = {el} />
+                    })
+                }
+            </ul>
+        );
+}
---
+++ ListItem.jsx
@@ -0,0 +1,11 @@
+function ListItem(props)
+{
+    return (
+        <li>
+            <span className='task'>{props.taskVal}</span>
+            <button onClick={()=>{
+                props.removeTask(props.taskVal)
+            }}>X</button>
+        </li>
+    )
+}
